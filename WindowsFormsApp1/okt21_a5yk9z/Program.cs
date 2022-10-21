9c9549b7bcbcb21cf3b025ecb92bb9d752a6f1c0
Plasius <atiiworks@gmail.com>
2022-10-21 09:49:01 +0200

    new project

---
+++ WindowsFormsApp1/okt21_a5yk9z/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okt21_a5yk9z
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
