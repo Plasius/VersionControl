852506f2de15661cbf66b5d9fc4146b610cdcf39
Plasius <atiiworks@gmail.com>
2022-11-04 10:55:47 +0100

    abstractions, car class

---
+++ WindowsFormsApp1/Santa/Entities/Ball.cs
@@ -0,0 +1,21 @@
+﻿using Santa.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santa.Entities
+{
+    public class Ball : Toy
+    {
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+    }
+}
