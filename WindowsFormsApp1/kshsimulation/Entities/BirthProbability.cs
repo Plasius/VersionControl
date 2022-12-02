1fa1d4721e7bf6cc0ec57dbbed37c67a9a9313c3
Plasius <atiiworks@gmail.com>
2022-12-02 10:40:16 +0100

    csvk

---
+++ WindowsFormsApp1/kshsimulation/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kshsimulation.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+
+        public int Children { get; set; }
+
+        public double P { get; set; }
+    }
+}
